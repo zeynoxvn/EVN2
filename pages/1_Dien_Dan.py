f64a304f141f48d3af352b2375555a2f27b82a9b
zeynoxvn <luongle2510@gmail.com>
2026-08-02 21:12:28 +0700

    Update 1_Dien_Dan.py

--- pages/1_Dien_Dan.py
+++ pages/1_Dien_Dan.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import requests
+# Đặt đoạn này ở đầu các file trang con trong thư mục pages/
+if not st.session_state.get("logged_in", False):
+    st.error("⚠️ Bạn chưa đăng nhập! Vui lòng quay lại trang chính để đăng nhập trước khi sử dụng tính năng này.")
+    st.page_link("app.py", label="🔑 Quay lại Trang chủ để Đăng nhập", icon="👉")
+    st.stop() # Dừng không cho chạy tiếp nội dung bên dưới
 import sys
 import os
 # Chỉ đường cho Python mò ra thư mục gốc
