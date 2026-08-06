d1807aad80bb73dbeaca3b0b4343c53d1e9ebd17
zeynoxvn <luongle2510@gmail.com>
2026-08-06 20:22:37 +0700

    Update 7_San_Dau.py

--- pages/7_San_Dau.py
+++ pages/7_San_Dau.py
@@ -10,7 +10,7 @@ st.page_link("app.py", label="🏠 Quay lại Trang chủ", icon="⬅️")
 st.markdown("---") # Kẻ một đường ngang cho tách biệt
 
 # --- ĐIỀN LINK API CỦA FEN VÀO ĐÂY ---
-API_URL = "LINK_WEB_APP_APPS_SCRIPT_CUA_FEN" 
+API_URL = "https://script.google.com/macros/s/AKfycbzV0KqHng6Edeb8LupXLSY84M_v4VnenGHenVWj_d7pvzVlsq2KWwh7dN-xwOSP33oh/exec" 
 
 # ==========================================
 # 2. KHÓA CỬA SÀN ĐẤU (NẾU CHƯA ĐĂNG NHẬP)
